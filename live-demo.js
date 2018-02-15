d2ed68717f3e48ae00f53c71a614508344540daf
Zach Bauer <zbauer91@gmail.com>
2018-02-15 16:09:24 -0500

    added the js demo, removed text from ws demo. nixed autoformat

---
+++ live-demo.js
@@ -0,0 +1,49 @@
+   				
+function factorialize(num) { 	  	   
+	var factorial = 1 			 
+						
+	for(let i=2; i<=num; i++) {		 	
+					factorial*=i  
+	}    		  
+	 console.log(factorial)	 		  
+						 
+}   		 				
+						
+factorialize(10)   	 		  
+						
+	 //All this program does is factorials
+	//It's the truth, I swear	   
+					 
+						
+							
+					 
+					 
+					 
+							
+					
+						
+					
+						
+	//Nothing to see here! 	 
+				 
+				
+				 
+			
+	 //Still nothing to see... 	
+							
+
+						
+
+			 
+		 
+		 
+
+								 
+
+
+//Hi again!	 	 	 
+
+							 
+
+
+
